327fbcb570df40f633460d0b7177f572f028d9fd
vietho <vietho@linecorp.com>
2025-06-28 11:47:55 +0700

    add test, fix splash to dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <script setup lang="ts">
-import {RouterView, useRouter} from 'vue-router'
+import {RouterView} from 'vue-router'
 import AppLayout from "@/layout/AppLayout.vue";
-import WelcomeView from "@/views/WelcomeView.vue";
-import {onMounted, ref} from 'vue'
-
-const showWelcome = ref(true);
-const router = useRouter()
-
-onMounted(() => {
-    setTimeout(() => {
-        showWelcome.value = false
-        router.replace('/') // change '/main' to your actual route
-    }, 500)
-})
+import {ref} from 'vue'
 </script>
 
 <template>
-    <!--    <Loading v-if="isLoading"/>-->
-    <WelcomeView v-if="showWelcome"/>
-    <AppLayout v-else>
+    <AppLayout>
         <RouterView/>
     </AppLayout>
 </template>
